35c93ba062e39e6b6a8ba66b7bf18dd8a119c21d
adriiianiiitaj <adriana.love.21@gmail.com>
2015-10-18 22:45:53 -0500

    Agregando fuentes, borrando archivos innecesarios

---
+++ admin/index - copia.php
@@ -0,0 +1,189 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="apple-touch-icon" href="apple-touch-icon.png"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"/>
+  <link rel="stylesheet" href="../css/bootstrap.min.css"/>
+  <link rel="stylesheet" href="../css/bootstrap-theme.min.css"/>
+  <link rel="stylesheet" href="../css/normalize.css"/>
+  <link rel="stylesheet" href="../css/green.css"/>
+  <link rel="stylesheet" href="../css/custom.css"/>
+  <link rel="stylesheet" href="../css/settings.css"/>
+  <!--[if lt IE 9]>
+      <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
+  <![endif]-->
+</head>
+<body class="nav-md">
+  <!--[if lt IE 8]>
+      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
+  <div class="container body">
+    <div class="main_container">
+      <div class="col-md-3 left_col">
+        <div class="left_col scroll-view">
+          <div class="navbar nav_title" style="border: 0;">
+            <a href="index.html" class="site_title"><span></span></a>
+          </div>
+
+          <!-- menu prile quick info -->
+          <div class="profile">
+            <div class="profile_info">
+              <span>Welcome,</span>
+              <h2>Anthony Mendoza</h2>
+            </div>
+          </div>
+          <!-- /menu prile quick info -->
+          <div class="clearfix"></div>
+
+      
+          <!-- sidebar menu -->
+          <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+            <div class="menu_section">
+              <h2>Menu</h2>
+              <ul class="nav side-menu">
+                <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-right"></span></a></li>
+                <li><a><i class="fa fa-users"></i> Users <span class="fa fa-chevron-right"></span></a></li>
+                <li><a><i class="fa fa-gift"></i> Products <span class="fa fa-chevron-right"></span></a></li>
+                <li><a><i class="fa fa-tags"></i> Categories <span class="fa fa-chevron-right"></span></a></li>
+                <li><a><i class="fa fa-tachometer"></i> Dashboard <span class="fa fa-chevron-right"></span></a></li>
+                <li><a><i class="fa fa-video-camera"></i> Multimedia <span class="fa fa-chevron-right"></span></a>
+                  <!--
+                  <ul class="nav child_menu" style="display: none">
+                    <li><a href="tables.html">Tables</a>
+                    </li>
+                    <li><a href="tables_dynamic.html">Table Dynamic</a>
+                    </li>
+                  </ul>-->
+                </li>
+              </ul>
+            </div>
+
+            <div class="menu_section">
+              <h2>My Account</h2>
+              <ul class="nav side-menu">
+                <li><a><i class="fa fa-user"></i> My Account <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu">
+                    <li><a href="e_commerce.html">Personal info</a>
+                    </li>
+                    <li><a href="projects.html">Change Password</a>
+                    </li>
+                  </ul>
+                </li>
+              <!--
+                <li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu" style="display: none">
+                        <li><a href="page_404.html">404 Error</a>
+                        </li>
+                        <li><a href="page_500.html">500 Error</a>
+                        </li>
+                        <li><a href="plain_page.html">Plain Page</a>
+                        </li>
+                        <li><a href="login.html">Login Page</a>
+                        </li>
+                        <li><a href="pricing_tables.html">Pricing Tables</a>
+                        </li>
+                    </ul>
+                </li>-->
+                <li><a><i class="fa fa-shopping-cart"></i> Orders <span class="label label-success pull-right"> New </span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- /sidebar menu -->
+
+          <!-- /menu footer buttons -->
+          <div class="sidebar-footer hidden-small">
+            <a data-toggle="tooltip" data-placement="top" title="Settings">
+              <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+              <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="Lock">
+              <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="Logout">
+              <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+            </a>
+          </div>
+          <!-- /menu footer buttons -->
+        </div>
+      </div>
+
+      <!-- top navigation -->
+      <div class="top_nav">
+        <div class="nav_menu">
+          <nav class="" role="navigation">
+            <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+            </div>
+
+            <ul class="nav navbar-nav navbar-right">
+              <li class="">
+                <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                  <img src="images/img.jpg" alt="">John Doe
+                  <span class=" fa fa-angle-down"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
+                  <li><a href="javascript:;">  Personal Info </a>
+                  </li>
+                  <li><a href="javascript:;">  Change Password </a>
+                  </li>
+                  <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <!-- /top navigation -->
+
+      <!-- page content -->         
+      <div class="right_col" role="main">  
+        <div class="row">
+          <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="x_panel fixed_height_full">
+              <div class="x_title">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+              
+        <!-- footer content -->
+        <footer>
+          <div class="">
+            <p class="pull-right">Proyect by |
+              <span class="lead"> <i class="fa fa-heart"></i> Adriana </span>
+            </p>
+          </div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+      <!-- /page content -->
+
+    </div>
+  </div>
+
+  <div id="custom_notifications" class="custom-notifications dsp_none">
+    <ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
+    </ul>
+    <div class="clearfix"></div>
+    <div id="notif-group" class="tabbed_notifications"></div>
+  </div>    
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.js"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.js"><\/script>')</script>
+  <script src="../js/vendor/bootstrap.min.js"></script>
+  <script src="../js/plugins.js"></script>
+  <script src="../js/main.js"></script>
+</body>
+</html>
